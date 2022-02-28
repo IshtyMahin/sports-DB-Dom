b7d8138d3fb35333838afa139e1a4b2d2757985b
Ishtiaq uddin <ishty119@gmail.com>
2022-02-28 19:06:31 +0600

    api push

---
+++ app.js
@@ -0,0 +1,13 @@
+ const allPlayers =() => {
+     const searchValue = document.getElementById("search-box").value;
+
+    const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue} `;
+    console.log(url);
+    fetch(url)
+      .then(res => res.json())
+      .then(data => console.log(data));
+
+
+
+     console.log(searchValue);
+ }
